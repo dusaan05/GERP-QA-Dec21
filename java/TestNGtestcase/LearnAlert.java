f5e63ebbf7310b94737885fa8ef7695d807f1033
dusaan05 <91422692+dusaan05@users.noreply.github.com>
2021-12-29 07:04:55 -0500

    Add files via upload

---
+++ java/TestNGtestcase/LearnAlert.java
@@ -0,0 +1,57 @@
+package TestNGtestcase;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnAlert {
+
+	@Test
+	public void learnAlert() {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		driver.switchTo().alert().accept();
+		
+		String output = driver.findElement(By.id("result")).getText();
+		
+		if(output.contains("You successfully clicked an alert"))
+		{
+			
+			System.out.println("Pass");
+				
+		}
+		else
+			System.out.println("Output is not the expected result");
+
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		driver.switchTo().alert().dismiss();
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		driver.switchTo().alert().accept();
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		driver.switchTo().alert().sendKeys("Hi, I am JavaScript");
+		Alert promptAlert = driver.switchTo().alert();
+		promptAlert.accept();
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		driver.switchTo().alert().sendKeys("Hi, I am JavaScript");
+		promptAlert.dismiss();	
+		System.out.println("PromtAlert Completed");
+		
+		
+		
+	
+		
+		
+
+	}
+
+}
---
+++ java/TestNGtestcase/LearnFrame.java
@@ -0,0 +1,43 @@
+package TestNGtestcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnFrame {
+
+	@Test
+	public void runLearnFrame() {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://chercher.tech/practice/frames-example-selenium-webdriver");
+		driver.manage().window().maximize();
+
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.tagName("input")).sendKeys("Hi, this is Sudhakar here");
+		
+		driver.switchTo().frame("frame3");
+		driver.findElement(By.id("a")).click();
+		
+		//Syntax for going back to your parent frame window
+		driver.switchTo().parentFrame();
+		driver.findElement(By.tagName("input")).clear();
+		
+		//switch to the home window (html DOM page)
+		driver.switchTo().defaultContent();
+		
+		String text = driver.findElement(By.xpath("//span[text()='Not a Friendly Topic']")).getText();
+		System.out.println(text);
+				
+		driver.switchTo().frame("frame2");
+		Select selectAnimals = new Select (driver.findElement(By.id("animals")));
+		selectAnimals.selectByVisibleText("Avatar");
+		
+		
+	}
+
+}
